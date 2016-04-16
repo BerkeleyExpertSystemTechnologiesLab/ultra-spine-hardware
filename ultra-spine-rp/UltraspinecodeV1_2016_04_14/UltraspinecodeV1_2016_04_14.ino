e5ad0afb4d312d89522683a17f10152d52f366bb
LARA JANSE VAN VUUREN <lavv16@gmail.com>
2016-04-15 23:51:38 -0700

    UltraSpine CalDay code with motor direction and motor designation matched to current robot

--- ultra-spine-rp/UltraspinecodeV1_2016_04_14/UltraspinecodeV1_2016_04_14.ino
+++ ultra-spine-rp/UltraspinecodeV1_2016_04_14/UltraspinecodeV1_2016_04_14.ino
@@ -9,8 +9,8 @@
 //SoftwareSerial mySerial=SoftwareSerial(rxpin,txpin);
 
 // Set values of pins for CW and CCW motor directions
-int motor3A = 3;  //(+)?
-int motor3B = 4; //(-)?
+int motor3A = 3;  
+int motor3B = 4; 
 int motor1B = 5; 
 int motor1A = 6; 
 int motor2A = 20;
@@ -18,58 +18,69 @@ int motor2B = 21;
 int motor4B = 22; 
 int motor4A = 23;
 int led = 13; 
-float delayMotion = 400;
+float delayMotion = 10000;
 void setup() {
   
   // Declare pins  
-  pinMode(motor1A, OUTPUT); // To BIN1 on motor driverA
-  pinMode(motor1B, OUTPUT); // To BIN2 on on motor driverA
-  pinMode(motor3B, OUTPUT); // To AIN2 on motor driverA
-  pinMode(motor3A, OUTPUT); // To AIN1 on motor driverA
+  pinMode(motor1A, OUTPUT); 
+  pinMode(motor1B, OUTPUT); 
+  pinMode(motor3B, OUTPUT); 
+  pinMode(motor3A, OUTPUT); 
 
-  pinMode(motor2A, OUTPUT); // To AIN1 on motor driverB
-  pinMode(motor2B, OUTPUT); // To AIN2 on on motor driverB
-  pinMode(motor4B, OUTPUT); // To BIN2 on motor driverB
-  pinMode(motor4A, OUTPUT); // To BIN1 on motor driverB
+  pinMode(motor2A, OUTPUT); 
+  pinMode(motor2B, OUTPUT); 
+  pinMode(motor4B, OUTPUT); 
+  pinMode(motor4A, OUTPUT); 
   pinMode(led, OUTPUT);  
   
 }
+//motor1 false (CW) - bottom left hind legs towards head
+//motor1 true (CCW) - bottom left hind legs away from head
+//motor2 false (CCW) - top left front legs away
+//motor2 true (CW) - top left front legs toward
+//motor3 true (CW) - bottom left hind legs toward
+//motor3 false (CCW) - bottom left hind legs away
+//motor4 **Check again!
 
 void loop() {
+LightFunction(); 
+Motor4(true,250); 
+LightFunction(); 
+delay(delayMotion); 
 
-   StopMotors();
-   LightFunction();
-   Motor1(true,200); 
-   delay(delayMotion);
-   StopMotors();
-   LightFunction();
-   Motor1(false,200);
-   delay(delayMotion);  
-   StopMotors(); 
-   LightFunction();
-   Motor2(true,200); 
-   delay(delayMotion);
-   StopMotors();
-   LightFunction();
-   Motor2(false,200);
-   delay(delayMotion);  
-   StopMotors(); 
-   LightFunction();
-   Motor3(true,200); 
-   delay(delayMotion);
-   StopMotors();
-   LightFunction();
-   Motor3(false,200);
-   delay(delayMotion);  
-   StopMotors(); 
-   LightFunction();
-   Motor4(true,200); 
-   delay(delayMotion);
-   StopMotors();
-   LightFunction();
-   Motor4(false,200);
-   delay(delayMotion);  
-   StopMotors(); 
+//   StopMotors();
+//   LightFunction();
+//   Motor1(true,200); 
+//   delay(delayMotion);
+//   StopMotors();
+//   LightFunction();
+//   Motor1(false,200);
+//   delay(delayMotion);  
+//   StopMotors(); 
+//   LightFunction();
+//   Motor2(true,200); 
+//   delay(delayMotion);
+//   StopMotors();
+//   LightFunction();
+//   Motor2(false,200);
+//   delay(delayMotion);  
+//   StopMotors(); 
+//   LightFunction();
+//   Motor3(true,200); 
+//   delay(delayMotion);
+//   StopMotors();
+//   LightFunction();
+//   Motor3(false,200);
+//   delay(delayMotion);  
+//   StopMotors(); 
+//   LightFunction();
+//   Motor4(true,200); 
+//   delay(delayMotion);
+//   StopMotors();
+//   LightFunction();
+//   Motor4(false,200);
+//   delay(delayMotion);  
+//   StopMotors(); 
 }
 
 void Motor1(bool dir, int spd){
