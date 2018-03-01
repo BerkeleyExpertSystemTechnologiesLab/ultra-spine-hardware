01dfb6706f8bb1ac60c223415cde5e2f1dd563c3
Andrew Sabelhaus <apsabelhaus@berkeley.edu>
2018-02-28 17:15:11 -0800

    Experiment sketch won't work, SerialEvent isn't actually interrupt driven. Need to rethink the whole thing or toss it. Kim's code is what we'll really be using for data collection.

--- laika_v1/iros2018_experiment/iros2018_experiment/iros2018_experiment.ino
+++ laika_v1/iros2018_experiment/iros2018_experiment/iros2018_experiment.ino
@@ -15,8 +15,8 @@
 // and break out of whatever else was running.
 // Else, if it's appropriate for the subroutine, just alert the caller that a command
 // was received. So, here's what we'll do:
-// (1) set up and interrupt for any serial terminal data, which can do something at a newline
-// (2) in that interrupt, call a function to check if the input was one of the main commands, return true or false
+// (1) set up and interrupt for any serial terminal data, which can do something at a newline. DONE
+// (2) in that interrupt, call a function to check if the input was one of the main commands, return true or false DONE
 // (3) in the main loop, just keep switching on whatever current command is, since the subroutine will control its own wait/timing
 // (4) In each subroutine, when we want to get a specific (not main) command, we do:
 //    (4.1) wait until new command complete (while loop)
@@ -31,39 +31,32 @@
 
 // Constants for pins with each motor
 // Motor 1: (assumed horizontal.)
-#define InA1_M1 = 6;                   // INA motor pin
-#define InB1_M1 = 5;                     // INB motor pin
-#define PWM1_M1 = 9;                 // PWM motor 1 pin
-#define encodPinA1_M1  = 2;                       // encoder A pin
-#define encodPinB1_M1 = 11;// encoder B pin
+#define InA1_M1 6                   // INA motor pin
+#define InB1_M1 5                     // INB motor pin
+#define PWM1_M1 9                 // PWM motor 1 pin
+#define encodPinA1_M1 2                       // encoder A pin
+#define encodPinB1_M1 11    // encoder B pin
 // Motor 2: (assumed center vert.)
-#define InA1_M2 = 7;
-#define InB1_M2 = 8;
-#define PWM1_M2 = 10;
-#define encodPinA1_M2 = 3;
-#define encodPinB1_M2 = 12;
+#define InA1_M2 7
+#define InB1_M2 8
+#define PWM1_M2 10
+#define encodPinA1_M2 3
+#define encodPinB1_M2 12
 
 // make it easier to remember direction of rotation:
 #define FORWARD         1                       // direction of rotation
 #define BACKWARD        2                       // direction of rotation
 
-// For switching between commands
-// Since we've already defined some above, start at 100.
-// TO-DO: check that these aren't something that would otherwise be put in as a command, like a double or something
-/*
-#define STOPALL 100 // will stop all motors
-#define STOP1 101   // only stop motor 1 or 2 (maybe unused.)
-#define STOP2 102
-#define RESET1 103  // reset the first motor to its original position
-#define RESET2 104
-#define MOVE1 105   // for when we're manually adjusting the horiz cables
-#define MOVE2 106   // maybe we want to manually adjust the center vert also
-#define RUNTEST 107 // run the test for the center vertebra
-*/
-// Also, we're going to set a value if a string input does NOT represent a command.
-//#define NOTACOMAMAND = 9999;
-
 int pwmVal = 100; // defaults to only 100/255 of the max power
+
+// for motor control: need interrupts for both motors, and checking
+// if each is supposed to be running. 1 = horiz, 2 = center
+boolean runh = false;
+boolean runc = false;
+// counting rotations for each motor, in both rad and encoder ticks
+long counth = 0;
+long countc = 0;
+// 
 
 // for storing comm data:
 String input;
@@ -86,14 +79,12 @@ boolean isMainCommand = true; // each subroutine will check and return to main l
 String commandString = ""; // default to no command
 
 // ...it makes things easier if we do this as an ENUM so we can avoid passing around strings as "commands."
-// POSSIBLECOMMANDS is an enum variable of something from this list
+// POSSIBLECOMMANDS is the name of the type of this enumeration
 typedef enum {STOPALL, STOPH, STOPC, RESETH, RESETC, RESETALL, MOVEH, MOVEC, RUNTEST, NOTACOMMAND} POSSIBLECOMMANDS;
-// an enum we store
+
+// an enum we store, of the type of the above, with the variable name commandEnum.
 POSSIBLECOMMANDS commandEnum = NOTACOMMAND; // default value will be do nothing. This is (type) (variable name) = (value).
 
-// If we need to iterate over enums: https://www.geeksforgeeks.org/enumeration-enum-c/, iterating over enums easier.
-
-//boolean sendCommand = false;
 
 // We'll be correlating timestamp with encoder values
 unsigned long lastMilli = 0;                    // loop timing
@@ -108,34 +99,32 @@ unsigned long lastMilliPrint = 0;               // loop timing
 // We're going to want to convert from string inputs to these enums, for easier looping.
 // So, do the following:
 //https://stackoverflow.com/questions/16844728/converting-from-string-to-enum-in-c
-// NOTE: SINCE OUR COMMANDS END WITH NEWLINES, NEED TO USE \n AT THE END OF ALL THESE
+
 const static struct {
     POSSIBLECOMMANDS commandAsEnum;
     String str;
 } conversion [] = {
-    {STOPALL, "stopall\n"},
-    {STOPH, "stoph\n"},
-    {STOPC, "stopc\n"},
-    {RESETH, "reseth\n"},
-    {RESETC, "resetc\n"},
-    {RESETALL, "resetall\n"},
-    {MOVEH, "moveh\n"},
-    {MOVEC, "movec\n"},
-    {RUNTEST, "runtest\n"}
+    {STOPALL, "stopall"},
+    {STOPH, "stoph"},
+    {STOPC, "stopc"},
+    {RESETH, "reseth"},
+    {RESETC, "resetc"},
+    {RESETALL, "resetall"},
+    {MOVEH, "moveh"},
+    {MOVEC, "movec"},
+    {RUNTEST, "runtest"}
 };
 
-// changed const char* to String everywhere
+// This function converts between strings coming from the terminal,
+// and our enum variable that's used to differentiate behaviors.
 POSSIBLECOMMANDS str2enum (String str)
 {
-  //Serial.println(str);
   // Will iterate through the conversion struct.
   int j;
   for (j = 0;  j < sizeof (conversion) / sizeof (conversion[0]);  ++j){
     // previously used strcmp, !strcmp (str, conversion[j].str)
     if (str == conversion[j].str) {
       // If we've found a match:
-      Serial.print("Assigning enum for string: ");
-      Serial.println(conversion[j].str);
       return conversion[j].commandAsEnum;     
     } 
   }
@@ -143,41 +132,113 @@ POSSIBLECOMMANDS str2enum (String str)
   return NOTACOMMAND;
 }
 
-
 // the executeCommand function is a short litle helper for switching over enums
 // and calling the right subroutine.
 void executeMainCommand(POSSIBLECOMMANDS commandToExecute) {
   // Switch on the enum
+  // Just before calling the subroutine, reset the command, since we've
+  // gotten our use from it right now.
+  // This makes it easier for the subroutines to read in new data, resetting commandComplete = 0;
   switch(commandToExecute){
     case STOPALL:
       Serial.println("Executing stopall");
+      resetCommand();
+      execute_stopall();
+      break;
+    case STOPH:
+      Serial.println("Executing stoph");
+      resetCommand();
+      execute_stoph();
+      break;
+    case STOPC:
+      Serial.println("Executing stopc");
+      resetCommand();
+      execute_stopc();
+      break;
+    case RESETALL:
+      Serial.println("Executing resetall");
+      resetCommand();
+      break;
+    case RESETH:
+      Serial.println("Executing reseth");
+      resetCommand();
+      break;
+    case RESETC:
+      Serial.println("Executing resetc");
+      resetCommand();
+      break;
+    case MOVEH:
+      Serial.println("Executing moveh");
+      resetCommand();
+      execute_moveh();
+      break;
+    case MOVEC:
+      Serial.println("Executing movec");
+      resetCommand();
+      break;
+    case RUNTEST:
+      Serial.println("Executing runtest");
+      resetCommand();
       break;
     default:
       Serial.println("not executing a command.");  
+      resetCommand();
   }
 }
 
+// a short helper: reset the command (both string and enum), so we won't forget.
+void resetCommand(){
+  commandString = "";
+  commandEnum = NOTACOMMAND;
+  commandComplete = false;
+}
+
+/**
+ * SETUP
+ * The setup function has the responsibility of configuring all pins.
+ */
 void setup() {
-  // put your setup code here, to run once:
+  // a nice instructional message
   Serial.begin(115200);
   Serial.println("Testing for IROS 2018 Laika results. Options: (lowercase)");
   Serial.println("stopall, stoph, stopc, reseth, resetc, moveh, movec, runtest");
   Serial.println("Remember, you need to set line ending to Newline in the serial monitor.");
+  // Configure all pins.
+  // For motor 1 (horizontal, h)
+  pinMode(InA1_M1, OUTPUT);
+  pinMode(InB1_M1, OUTPUT);
+  pinMode(PWM1_M1, OUTPUT);
+  pinMode(encodPinA1_M1, INPUT);
+  pinMode(encodPinB1_M1, INPUT);
+  digitalWrite(encodPinA1_M1, HIGH);                      // turn on pullup resistor
+  digitalWrite(encodPinB1_M1, HIGH);
+  // For motor 2 (center, c)
+  pinMode(InA1_M2, OUTPUT);
+  pinMode(InB1_M2, OUTPUT);
+  pinMode(PWM1_M2, OUTPUT);
+  pinMode(encodPinA1_M2, INPUT);
+  pinMode(encodPinB1_M2, INPUT);
+  digitalWrite(encodPinA1_M2, HIGH);                      // turn on pullup resistor
+  digitalWrite(encodPinB1_M2, HIGH);
 }
 
+/**
+ * MAIN LOOP
+ * Responsible for switching between modes / commands. All subroutines will return back to here
+ * once either (a) they're finished, or (b) they received one of the main commands while 
+ * going about their business.
+ */
 void loop() {
-  // put your main code here, to run repeatedly:
-  // print the string when a newline arrives:
+  // If nothing else is happening, wait until a string command has fully arrived
+  // over the serial terminal.
   if (commandComplete) {
-    Serial.println(commandString);
     // Now, we have a new command. Let's switch on that value.
-    // Finally, clear the string, reset the int:
-    //Serial.println(commandEnum);
     // Switch on the enum and perform behavior.
+    // Whatever subroutine is called via executeMainCommand will have control
+    // of the arduino, and this main loop will not be executed "for a while"
     executeMainCommand(commandEnum);
-    commandString = "";
-    commandEnum = NOTACOMMAND;
-    commandComplete = false;
+     // Finally, clear the string, reset the enum:
+    resetCommand();
   }
 }
 
@@ -185,24 +246,24 @@ void loop() {
 // This runs whenever data is available in the serial buffer.
 void serialEvent() {
   // data is available
+  //DEBUGGING
+  Serial.println("Serial event occurred");
   while (Serial.available()) {
     // get the new byte:
     char inChar = (char)Serial.read();
-    // add it to the inputString:
-    commandString += inChar;
-    // if the incoming character is a newline, set a flag so the main loop can
-    // do something about it:
+    // Check if we got a newline, and either return or add the next character.
     if (inChar == '\n') {
+      // if the incoming character is a newline, set a flag so the main loop can
+      // do something about it:
       commandComplete = true;
       // Now, we have a command. 
       // We'll check if this is one of the main commands, so that whatever is doing the command-checking
-      // will know to return back to the main loop. This function also sets the commandInt value.
+      // will know to return back to the main loop.
       isMainCommand = checkIfMainCommand(commandString);
-      // Moving it here as opposed to main loop because we want to do interrupts everywhere,
-      // and also, this allows us to differentiate between main commands "stop", etc. versus
-      // inputs to the individual functions (like a number in radians.)
-      // good programming practice is to pass this string around, even though it's global.
-      //parseCommand(command);
+    }    
+    else {
+    // add it to the inputString:
+    commandString += inChar; 
     }
   }
 }
@@ -212,19 +273,86 @@ bool checkIfMainCommand(String possibleMain){
   // value to return
   bool isMain = 0;
   // Use the conversion function to check against our enumeration
-  // already declared this global variable
+  // already declared commandEnum as a global variable
   commandEnum = str2enum(possibleMain); 
-  //Serial.println(possibleMain);
-  // No more need for looping! If the convert function returned NOTACOMMAND then this value is easily set.
+  // Looping and checking occurs in str2enum! If the convert function returned NOTACOMMAND then this value is easily set.
   if( commandEnum != NOTACOMMAND){
     isMain = 1;
-    //Serial.print("Found a main command. Passed in:");
-    //Serial.println(possibleMain);
   }
   return isMain;
 }
 
-// The parseCommand function switches on a command string.
-// Primarily, it checks if any of the main commands were run, and
-// breaks out of whatever function was previously running
+/**
+ * Some helpers for motor control.
+ */
+void brakeMotor1(){
+  analogWrite(PWM1_M1, 0);
+  digitalWrite(InA1_M1, HIGH);
+  digitalWrite(InB1_M1, HIGH);
+}
+void brakeMotor2(){
+  analogWrite(PWM1_M2, 0);
+  digitalWrite(InA1_M2, HIGH);
+  digitalWrite(InB1_M2, HIGH);
+}
+/*
+void moveMotorH(int direction, int PWM_val, long tick)  {
+  runh = true;
+  //countInit = count;    // abs(count)
+  //tickNumber = tick;
+  if (direction == FORWARD)          motorForward(PWM_val);
+  else if (direction == BACKWARD)    motorBackward(PWM_val);
+}
+*/
+
+/**
+ * FUNCTIONS FOR EACH COMMAND
+ * Below, we have the behavior for each command that's input.
+ * A quick note about how data is read from the serial terminal from here out:
+ * Say, for example, one of these functions is running, and expects an angle from the terminal.
+ * We'd wait until commandComplete (the global bool) is set, maybe a while loop.
+ * Then, check if isMainCommand, in which case, it's this functions responsibility
+ * to return to the main loop (just call return.)
+ */
+void execute_stopall(){
+  // Stopping all the motors is braking both.
+  brakeMotor1();
+  brakeMotor2();
+}
+ 
+// Motor 1 is horizontal, 2 is center.
+void execute_stoph(){
+  brakeMotor1(); 
+}
+void execute_stopc(){
+  brakeMotor2();
+}
+
+// Manually moving either motor.
+void execute_moveh(){
+  // Here, we get an amount, in rad, to move.
+  Serial.println("Enter an amount in radians for horizontal motor (M1) to move:");
+  // keep checking until we get a new command, and confirm that it's not something
+  // that should break out to the main loop
+  while( !commandComplete ){
+    // do nothing
+    delay(100);
+    //Serial.println("Waiting patiently...");
+  }
+  //DEBUGGING
+  Serial.println(commandString);
+  // Next, check if break out:
+  if( isMainCommand ){
+    // go back and stop doing what we're doing!
+    return;
+  }
+  // OK! Great. Let's assume we've got a double.
+  double radians = commandString.toDouble();
+  //DEBUGGING: print the double.
+  Serial.println(radians);
+
+  // ...finally, once this is complete, need to reset the command entirely,
+  // for the main loop to operate properly.
+  resetCommand();
+}
 
